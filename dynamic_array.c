31019ac52e98651aabdc22233935505b0cf37b38
Vansh <valawat@purdue.edu>
2026-06-16 10:23:15 -0400

    dynamic array updated -> append functionality and accessing elements

--- dynamic_array.c
+++ dynamic_array.c
@@ -1,5 +1,6 @@
-#include<std.h>
+#include<stdio.h>
 #include<stdlib.h>
+#include<errno.h>
 
 typedef struct 
 {
@@ -10,15 +11,44 @@ typedef struct
 
 dynamArr* arrCreate(int cap, int len)
 {
-	dynamArr* r = (dynamArr*)malloc(sizeof(Rectangle));
-	if (r != NULL)
+	dynamArr* r = (dynamArr*)malloc(sizeof(dynamArr);
+			if (r != NULL)
+			{
+			r->capacity = cap;
+			r->length = len;
+			r->array = malloc(sizeof(int) * cap);
+			}
+
+			return r;
+			}
+
+			int access_arr(const dynamArr* self, int id)
+			{
+			if (id >= self->length || id < 0)
+			{
+			return ERANGE;
+			}
+
+			return self->array[id];
+			}
+
+void append(dynamArr* self, int elem)
+{
+	int* new_loc;
+	if (self->length >= self->capacity)
 	{
-		r->capacity = cap;
-		r->length = len;
-		r->array = malloc(sizeof(int)*cap);
+		self->capacity *= 2; 
+		new_loc = realloc(self->array, sizeof(int) * self->capacity);
+		if (!new_loc)
+		{
+			printf("Memory Allocation Failed");
+			self->capacity /= 2;
+			return;
+		}
+		self->array = new_loc;
 	}
+	self->array[self->length] = elem;
+	self->length++;
 
-	return r;
 }
-
 
