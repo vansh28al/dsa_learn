26eb7940f62d50923350f5001af469fb077c2d1b
Vansh <valawat@purdue.edu>
2026-06-16 22:11:57 -0400

    dynamic array updated -> freeing, searching, insertion functionalities added

--- dynamic_array.c
+++ dynamic_array.c
@@ -91,3 +91,61 @@ void del(dynamArr* self, int id)
 	}
 }
 
+void insert_at(dynamArr* self, int elem, int id)
+{
+	if (id >= self->length)
+	{
+		printf("Invalid index");
+		return;
+	}
+	int* new_loc;
+        if (self->length >= self->capacity)
+        {
+                self->capacity *= 2;
+                new_loc = realloc(self->array, sizeof(int) * self->capacity);
+                if (!new_loc)
+                {
+                        printf("Memory Allocation Failed");
+                        self->capacity /= 2;
+                        return;
+                }
+                self->array = new_loc;
+        }
+
+	int i;
+	for (i = self->length; i > id; i--)
+	{
+		self->array[i] = self->array[i-1];
+	}
+	self->array[id] =  elem;
+	self->length++;
+}
+
+
+void free_array(dynamArr* self)
+{
+	if (self != NULL)
+	{
+		free(self->array);
+		self->array = NULL;
+		self->length = 0;
+		self->capacity = 0;
+	}
+}
+
+int find_elem(dynamArr* self, int elem)
+{
+	int found = 0;
+	int i = -1;
+	while ((i < self->length) & !found)
+	{
+		i++;
+		if (self->array[i] == elem)
+		{
+			found = 1;
+		}
+	}
+	return i;
+}
+		
+
